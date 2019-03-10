bdc230fed7d8b4c6044ae3c1017896307e7dbffd
Shiyudada <1009269137@qq.com>
2019-03-10 11:15:42 +0800

    login页面

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+// 导入饿了么ui
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
 
 
 // 导入暴露的路由
 import router from './lib/router'
+
 new Vue({
   render: h => h(App),
   router
